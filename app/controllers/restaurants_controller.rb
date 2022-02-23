89f9586e3ca9a6d46e0be9de753379edd0039801
gabriel tavares <gabriel29t@gmail.com>
2022-02-23 14:28:25 -0300

    Challenge solved

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,54 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show chef edit update destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit; end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # ----------------------------------> STOP HERE
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Strong Params
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone,category: %w[chinese italian japanese french belgian].sample)
+  3.times do
+    Review.create(rating: rand(0..5), content: Faker::Restaurant.review, restaurant_id: restaurant.id)
+  end
+end
